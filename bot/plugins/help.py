7ad644d2b3af19ca7d2d3208441a31ed380502bb
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-06-15 06:14:03 -0700

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -41,7 +41,7 @@ def map(pos):
 
         button = [
             [
-             InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
+             InlineKeyboardButton(text = 'Support Chat', url = "https://t.me/lkhitech"),
              InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/kjeymax/GDUPLOAD_BOT2")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
