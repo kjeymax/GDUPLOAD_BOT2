a1c49fc93d76f71f201fbf2bb01edc898bea3485
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-06-15 06:14:54 -0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,6 @@ class config:
     API_HASH = ""
     DATABASE_URL = ""
     SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
     G_DRIVE_CLIENT_SECRET = ""
