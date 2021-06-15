69f90f9c179f33e971cd31fd32de3e00d8491b61
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-06-15 06:12:17 -0700

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -42,7 +42,7 @@ def map(pos):
         button = [
             [
              InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/viperadnan-git/google-drive-telegram-bot/issues/new")
+             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/kjeymax/GDUPLOAD_BOT2")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
